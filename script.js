9b2791bcc24ad190510fe9695f46f9526d5fa65c
Anwaar <hafizqazianwaar786@gmail.com>
2026-07-28 13:27:45 +0500

    machine styling works perfectly now moving to text animation

--- script.js
+++ script.js
@@ -5,6 +5,18 @@
 
 const storySection = document.querySelector("main");
 const machine = document.querySelector(".machine");
+const intro = document.querySelector(".intro");
+
+const right1 = document.querySelector(".right1");
+const left1 = document.querySelector(".left1");
+const right2 = document.querySelector(".right2");
+const left2 = document.querySelector(".left2");
+machine.style.transform = `
+    perspective(1600px)
+    rotateX(50deg)
+    translateY(0)
+    translateX(0)
+`;
 // ==========================================
 // Scroll Event
 // ==========================================
@@ -50,67 +62,101 @@ function updateStory() {
     // Scene 1 (0% - 20%)
     // ==========================================
 
-    if (progress <= 0.20) {
-
-        const p = progress / 0.20;
-
-        // Add your animation here
-        machine.style.animation="moveRight 1.15s ease forwards"
-
-    }
-
-
-
     // ==========================================
-    // Scene 2 (20% - 40%)
-    // ==========================================
+// Scene 1 (0% - 25%)
+// ==========================================
 
-    else if (progress <= 0.40) {
+if (progress <= 0.25) {
 
-        const p = (progress - 0.20) / 0.20;
+    let p = (progress - 0.10) / 0.05;
 
-        // Add your animation here
-                machine.style.animation="moveLeft 1.15s ease forwards"
+    p = Math.max(0, Math.min(p, 1));
 
+    const rotate = 50 - (50 * p);
+    const translate = 250 * p;
 
-    }
+    machine.style.transform = `
+        perspective(1600px)
+        rotateX(${rotate}deg)
+        translateY(0)
+        translateX(${translate}px)
+    `;
 
+     intro.classList.add("hide");
+showContent(right1);
 
+   
+}
+// ==========================================
+// Scene 2 (25% - 50%)
+// ==========================================
 
-    // ==========================================
-    // Scene 3 (40% - 60%)
-    // ==========================================
-
-    else if (progress <= 0.60) {
-
-        const p = (progress - 0.40) / 0.20;
-
-        // Add your animation here
-        machine.style.animation="moveRight2 1.15s ease forwards"
-
-    }
-
-
-
-
-    // ==========================================
-    // Scene 4 (60% - 80%)
-    // ==========================================
-
-    else if (progress <= 0.80) {
-
-        const p = (progress - 0.60) / 0.20;
-
-        // Add your animation here
-        machine.style.animation="moveLeft 1.15s ease forwards"
+else if (progress <= 0.50) {
+       
 
 
-    }
+    let p = (progress - 0.35) / 0.05;
+
+    p = Math.max(0, Math.min(p, 1));
+
+    const translate = 250 - (500 * p);
+
+    machine.style.transform = `
+        perspective(1600px)
+        rotateX(0deg)
+        translateX(${translate}px)
+       
+    `;
+
+  
+}
+
+// ==========================================
+// Scene 3 (50% - 75%)
+// ==========================================
+
+else if (progress <= 0.75) {
+
+    let p = (progress - 0.60) / 0.05;
+
+    p = Math.max(0, Math.min(p, 1));
+
+    const translate = -250 + (500 * p);
+
+    machine.style.transform = `
+        perspective(1600px)
+        rotateX(0deg)
+        translateX(${translate}px)
+        
+    `;
+
+   
+}
+// ==========================================
+// Scene 4 (75% - 100%)
+// ==========================================
+
+else {
+
+    let p = (progress - 0.85) / 0.05;
+
+    p = Math.max(0, Math.min(p, 1));
+
+    const translate = 250 - (500 * p);
+
+    machine.style.transform = `
+        perspective(1600px)
+        rotateX(0deg)
+        translateX(${translate}px)
+        
+    `;
+
     
-    else {
+}
+// ==========================================
+// Scene 5 (80% - 100%)
+// ==========================================
+
 
     
-
-    }
-
 }
